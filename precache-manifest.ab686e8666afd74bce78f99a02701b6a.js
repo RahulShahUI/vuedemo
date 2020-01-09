98eb564551a9037ddb553e113e2c81c6d4d52963
Rahul Shah <rahul.shah@credencys.com>
2020-01-09 15:23:21 +0530

    pwa added

---
+++ precache-manifest.ab686e8666afd74bce78f99a02701b6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4764df76eee2b9f050e2",
+    "url": "css/app.057ecc28.css"
+  },
+  {
+    "revision": "34957149e03e1f8319b7569e4dd649c2",
+    "url": "index.html"
+  },
+  {
+    "revision": "4764df76eee2b9f050e2",
+    "url": "js/app.8019de2b.js"
+  },
+  {
+    "revision": "5e727348b2da7f553c6c",
+    "url": "js/chunk-vendors.b6b2dd8c.js"
+  },
+  {
+    "revision": "642458fcd231b926080d62e048821e9d",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
